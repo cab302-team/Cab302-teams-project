ebec62e04ea792a992ed66fe74cc6b3f447321f4
LC-QUT <l6.carroll@connect.qut.edu.au>
2025-09-01 16:16:23 +1000

    added tile

---
+++ src/main/java/com/example/project/Tile.java
@@ -0,0 +1,31 @@
+package com.example.project;
+
+public class Tile {
+
+    private final char letter;
+
+    private final int value;
+
+
+    public Tile(char letter, int value) {
+
+        this.letter = letter;
+
+        this.value = value;
+
+    }
+
+
+    public char getLetter(){
+
+        return letter;
+
+    }
+
+    public int getValue(){
+
+        return value;
+
+    }
+
+}
