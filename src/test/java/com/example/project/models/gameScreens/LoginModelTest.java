fe45e04773b9481dadfa27d1ab1fa1b926ca172c
matilda-greening <tilly.greening@outlook.com>
2025-09-18 00:12:22 +1000

    login model test created

---
+++ src/test/java/com/example/project/models/gameScreens/LoginModelTest.java
@@ -0,0 +1,32 @@
+package com.example.project.models.gameScreens;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LoginModelTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void isValidLogin_False() {
+    }
+
+    @Test
+    void isValidLogin_True() {
+    }
+
+    @Test
+    void loginUser() {
+    }
+
+    @Test
+    void isSignedUp() {
+    }
+
+    @Test
+    void signUp() {
+    }
+}
