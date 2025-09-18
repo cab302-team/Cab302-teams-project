98cda9e3740ca269fb8821efb960814d95668945
LC-QUT <l6.carroll@connect.qut.edu.au>
2025-09-18 15:45:30 +1000

    made definitionView

---
+++ src/main/java/com/example/project/controllers/popupControllers/DefinitionController.java
@@ -0,0 +1,11 @@
+package com.example.project.controllers.popupControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+/**
+ * Controller for definition popup
+ */
+public class DefinitionController {
+    @FXML private ImageView definitionImage;
+}
