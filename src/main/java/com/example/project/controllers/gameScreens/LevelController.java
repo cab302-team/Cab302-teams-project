e704338d4ecd30fc65e4e67027ce3fe9edd4368f
Clarkson <graceclarkson1415@gmail.com>
2025-09-18 10:12:34 +1000

    Update LevelController.java

--- src/main/java/com/example/project/controllers/gameScreens/LevelController.java
+++ src/main/java/com/example/project/controllers/gameScreens/LevelController.java
@@ -254,6 +254,7 @@ public class LevelController extends GameScreenController
     private void onRedrawButton()
     {
         levelModel.setIsRedrawActive(!levelModel.getIsRedrawActive().get());
+        levelModel.returnRedrawTilesToTheRack();
     }
 
     /**
