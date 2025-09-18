adc2b200cf3c8c730ed33016aca86a057665f007
LC-QUT <l6.carroll@connect.qut.edu.au>
2025-09-18 19:45:05 +1000

    popupLoader

--- src/main/java/com/example/project/services/PopupLoader.java
+++ src/main/java/com/example/project/services/PopupLoader.java
@@ -1,4 +1,47 @@
 package com.example.project.services;
 
-public class PopupLoader {
+import com.example.project.controllers.popupControllers.DefinitionController;
+import com.example.project.controllers.popupControllers.PopupController;
+import com.example.project.models.popups.DefinitionPopup;
+import com.example.project.models.popups.Popup;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+/**
+ * class to load the popup and initialize it with a controller
+ */
+public class PopupLoader
+{
+
+    /**
+     * create popup controller.
+     * @param popupObject popup object.
+     * @param <C> class of popup controller.
+     * @param <P> class of popup.
+     * @return returns controller of the tile.
+     */
+    private static <C extends PopupController<P>, P extends Popup> C createPopupController(P popupObject)
+    {
+        try
+        {
+            String fxmlPath = popupObject.getFXMLPath();
+            FXMLLoader loader = new FXMLLoader(PopupLoader.class.getResource(fxmlPath));
+            Node node = loader.load();
+            C controller = loader.getController();
+            controller.initialize(popupObject);
+            return controller;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create popup controller: " + popupObject.getFXMLPath(), e);
+        }
+    }
+
+    /**
+     * @param pup popup model
+     * @return returns definition popup controller
+     */
+    public static DefinitionController createDefinitionPopup(DefinitionPopup pup)
+    {
+        return createPopupController(pup);
+    }
+
 }
