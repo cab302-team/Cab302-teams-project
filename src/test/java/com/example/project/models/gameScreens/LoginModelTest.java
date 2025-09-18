8d8a7a26fa976e970490cdc56ec92ff69b129841
matilda-greening <tilly.greening@outlook.com>
2025-09-18 15:33:51 +1000

    tests done

--- src/test/java/com/example/project/models/gameScreens/LoginModelTest.java
+++ src/test/java/com/example/project/models/gameScreens/LoginModelTest.java
@@ -3,54 +3,111 @@ package com.example.project.models.gameScreens;
 import com.example.project.models.User;
 import com.example.project.services.*;
 import com.example.project.services.sqlite.dAOs.UsersDAO;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class LoginModelTest {
 
-    @Mock private UsersDAO mockUsersDAO;
-    @Mock private PasswordHasher mockPasswordHasher;
-    @Mock private Session mockSession;
-    @Mock private SceneManager mockSceneManager;
-    @InjectMocks private LoginModel loginModel;
-
     private final String Username1 = "Gustave";
     private final String Password1 = "ForThoseWhoComeAfter!33";
 
-
-    @Test
-    void getWelcomeTextProperty() {
-
-    }
-
     @Test
     void onLoginClicked_ValidUser() {
+        var mockUsersDAO = mock(UsersDAO.class);
+        var mockPasswordHasher = mock(PasswordHasher.class);
+        var mockSession = mock(Session.class);
+        var mockSceneManager = mock(SceneManager.class);
 
-        User mockUser = new User(Username1, Password1, 0);
-
+        var loginModel = new LoginModel(mockSession, mockUsersDAO, mockPasswordHasher);
+        var user = new User(Username1, Password1, 0);
+        var sceneManager = new SceneManager(mockSceneManager);
 
         when(mockUsersDAO.doesUserExist(Username1))
                 .thenReturn(true);
-        when(mockUsersDAO.getUser(Username1)).thenReturn(mockUser);
-        when(mockPasswordHasher.doesPasswordMatch(Password1, mockUser.getPassword()))
+        when(mockUsersDAO.getUser(Username1)).thenReturn(user);
+        when(mockPasswordHasher.doesPasswordMatch(Password1, user.getPassword()))
                 .thenReturn(true);
 
         loginModel.onLoginClicked(Username1, Password1);
 
-        verify(mockUsersDAO).getUser(Username1);
-        verify(mockSession).setUser(mockUser);
+        verify(mockUsersDAO, times(2)).getUser(Username1);
+        verify(mockSession).setUser(user);
         verify(mockSceneManager).switchScene(GameScenes.LEVEL);
+    }
+
+    @Test
+    void onLoginClicked_InvalidUser() {
+        var mockUsersDAO = mock(UsersDAO.class);
+        var mockPasswordHasher = mock(PasswordHasher.class);
+        var mockSession = mock(Session.class);
+
+        var loginModel = new LoginModel(mockSession, mockUsersDAO, mockPasswordHasher);
+
+        when(mockUsersDAO.doesUserExist(Username1))
+                .thenReturn(false);
+
+        loginModel.onLoginClicked(Username1, Password1);
+        verify(mockUsersDAO).doesUserExist(Username1);
+        assertEquals("No account found. Sign up first.", loginModel.getWelcomeTextProperty().get());
+    }
+
+    @Test
+    void onLoginClicked_IllegalInput() {
+        var mockUsersDAO = mock(UsersDAO.class);
+        var mockPasswordHasher = mock(PasswordHasher.class);
+        var mockSession = mock(Session.class);
+
+        var loginModel = new LoginModel(mockSession, mockUsersDAO, mockPasswordHasher);
+
+        loginModel.onLoginClicked(" ", " ");
+        assertEquals("Fields cannot be empty.", loginModel.getWelcomeTextProperty().get());
     }
 
 
     @Test
-    void onSignUpClicked() {
+    void onSignUpClicked_Valid() {
+        var mockUsersDAO = mock(UsersDAO.class);
+        var mockPasswordHasher = mock(PasswordHasher.class);
+        var mockSession = mock(Session.class);
+
+        var loginModel = new LoginModel(mockSession, mockUsersDAO, mockPasswordHasher);
+
+        when(mockUsersDAO.doesUserExist(Username1))
+                .thenReturn(false);
+
+        loginModel.onSignUpClicked(Username1, Password1);
+        verify(mockUsersDAO).addUser(argThat(user -> user.getUsername().equals(Username1)
+                && user.getPassword().equals(Password1)));
+        assertEquals("User added successfully.", loginModel.getWelcomeTextProperty().get());
     }
 
+    @Test
+    void onSignUpClicked_Invalid() {
+        var mockUsersDAO = mock(UsersDAO.class);
+        var mockPasswordHasher = mock(PasswordHasher.class);
+        var mockSession = mock(Session.class);
+
+        var loginModel = new LoginModel(mockSession, mockUsersDAO, mockPasswordHasher);
+
+        when(mockUsersDAO.doesUserExist(Username1))
+                .thenReturn(true);
+
+        loginModel.onSignUpClicked(Username1, Password1);
+        assertEquals("Already signed up. Click login.", loginModel.getWelcomeTextProperty().get());
+    }
+
+    @Test
+    void onSignUpClicked_IllegalInput() {
+        var mockUsersDAO = mock(UsersDAO.class);
+        var mockPasswordHasher = mock(PasswordHasher.class);
+        var mockSession = mock(Session.class);
+
+        var loginModel = new LoginModel(mockSession, mockUsersDAO, mockPasswordHasher);
+
+        loginModel.onSignUpClicked(" ", " ");
+        assertEquals("Fields cannot be empty.", loginModel.getWelcomeTextProperty().get());
+    }
 
 }
