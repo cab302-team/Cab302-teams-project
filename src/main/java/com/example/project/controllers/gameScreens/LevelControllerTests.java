6457b29277ae0a695cf4602e7e69d373314c2844
Clarkson <graceclarkson1415@gmail.com>
2025-09-18 18:36:37 +1000

    Create LevelControllerTests.java

---
+++ src/main/java/com/example/project/controllers/gameScreens/LevelControllerTests.java
@@ -0,0 +1,60 @@
+package com.example.project.controllers.gameScreens;
+
+import com.example.project.models.gameScreens.LevelModel;
+import com.example.project.services.GameScenes;
+import com.example.project.services.SceneManager;
+
+
+class LevelControllerTests
+{
+
+//    @Test
+//    void testOnSkipButton_switchesToShop()
+//    {
+//        var sceneManagerMock = mock(SceneManager.class);
+//        var sceneManager = new SceneManager(sceneManagerMock);
+//
+//        var modelMock = mock(LevelModel.class);
+//        var levelController = new LevelController(modelMock);
+//
+//        levelController.onSkipButton();
+//
+//        verify(sceneManagerMock).switchScene(GameScenes.SHOP);
+//    }
+//
+//    @Test
+//    void testOnRedrawButton_togglesRedrawActive()
+//    {
+//
+//        var sceneManagerMock = mock(SceneManager.class);
+//        var sceneManager = new SceneManager(sceneManagerMock);
+//
+//        var modelMock = mock(LevelModel.class);
+//        var levelController = new LevelController(modelMock);
+//
+//        when(modelMock.getIsRedrawActive()).thenReturn(new javafx.beans.property.SimpleBooleanProperty(false));
+//
+//        levelController.onRedrawButton();
+//
+//        verify(modelMock).setIsRedrawActive(true);
+//        verify(modelMock).returnRedrawTilesToTheRack();
+//    }
+//
+//    @Test
+//    void testOnConfirmRedrawButton_redrawsTiles()
+//    {
+//        var sceneManagerMock = mock(SceneManager.class);
+//        var sceneManager = new SceneManager(sceneManagerMock);
+//
+//        var modelMock = mock(LevelModel.class);
+//        var levelController = new LevelController(modelMock);
+//
+//        when(modelMock.getIsRedrawActive()).thenReturn(new javafx.beans.property.SimpleBooleanProperty(false));
+//
+//        levelController.onConfirmRedrawButton();
+//
+//        verify(modelMock).setIsRedrawActive(true);
+//        verify(modelMock).redrawTiles();
+//    }
+
+}
