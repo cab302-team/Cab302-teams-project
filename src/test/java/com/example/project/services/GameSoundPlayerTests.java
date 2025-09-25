91daad877a78779de20d90ef19bcae7dbc389996
Clarkson <graceclarkson1415@gmail.com>
2025-09-25 12:58:22 +1000

    some tests

--- src/test/java/com/example/project/services/GameSoundPlayerTests.java
+++ src/test/java/com/example/project/services/GameSoundPlayerTests.java
@@ -1,6 +1,34 @@
 package com.example.project.services;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Tests for {@link GameSoundPlayer}
+ */
 public class GameSoundPlayerTests
 {
-    
+    @Test
+    void test_GameSoundPlayer_success()
+    {
+        var validPath = "/com/example/project/Sounds/Clack1.wav";
+        var soundPlayer = new GameSoundPlayer(validPath);
+    }
+
+    @Test
+    void test_GameSoundPlayer_throwsIllegalArgumentException(){
+        var path = "invalid-path";
+        assertThrows(IllegalArgumentException.class, () -> new GameSoundPlayer(path));
+    }
+
+    @Test
+    void test_getClip()
+    {
+        var validPath = "/com/example/project/Sounds/Clack1.wav";
+        var soundPlayer = new GameSoundPlayer(validPath);
+        var clip = soundPlayer.getClip();
+
+        assertEquals();
+    }
 }
