ea2182bda150059ef668d1da280ab4df18503ba6
Clarkson <graceclarkson1415@gmail.com>
2025-09-25 17:15:43 +1000

    Update GameMusicPlayerTests.java

--- src/test/java/com/example/project/services/GameMusicPlayerTests.java
+++ src/test/java/com/example/project/services/GameMusicPlayerTests.java
@@ -18,7 +18,24 @@ public class GameMusicPlayerTests
     @Test
     void test_createdGameMusic_success()
     {
-        var musicPlayer = new GameMusicPlayer();
+        try (MockedStatic<AudioSystem> mockedAudioSystem = mockStatic(AudioSystem.class))
+        {
+            Clip mockClip = mock(Clip.class);
+            AudioInputStream mockOriginalStream = mock(AudioInputStream.class);
+            AudioInputStream mockConvertedStream = mock(AudioInputStream.class);
+            AudioFormat mockFormat = mock(AudioFormat.class);
+
+            mockedAudioSystem.when(() -> AudioSystem.getAudioInputStream(any(BufferedInputStream.class)))
+                    .thenReturn(mockOriginalStream);
+            mockedAudioSystem.when(() -> AudioSystem.getAudioInputStream(any(AudioFormat.class), any(AudioInputStream.class)))
+                    .thenReturn(mockConvertedStream);
+            mockedAudioSystem.when(AudioSystem::getClip)
+                    .thenReturn(mockClip);
+
+            when(mockOriginalStream.getFormat()).thenReturn(mockFormat);
+
+            var newMusic = new GameMusicPlayer();
+        }
     }
 
     @Test
