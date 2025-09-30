c390d7dc59c7a8e1036f271b6a5bdc5b2c022cdc
Kainat-10 <punnymilkshake424@gmail.com>
2025-09-30 14:57:18 +1000

    shopcontroller unit tests update

--- src/test/java/com/example/project/controllers/gameScreens/ShopControllerTests.java
+++ src/test/java/com/example/project/controllers/gameScreens/ShopControllerTests.java
@@ -1,4 +1,64 @@
 package com.example.project.controllers.gameScreens;
 
-public class ShopControllerTests {
+import com.example.project.models.gameScreens.ShopModel;
+import com.example.project.models.tiles.UpgradeTile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+class ShopControllerTests {
+
+    private ShopController controller;
+    private ShopModel mockShopModel;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        controller = new ShopController();
+
+        // mocking the ShopModel
+        mockShopModel = mock(ShopModel.class);
+        var shopModelField = ShopController.class.getDeclaredField("shopModel");
+        shopModelField.setAccessible(true);
+        shopModelField.set(controller, mockShopModel);
+    }
+
+    @Test
+    void onSceneChangedToThis_ShouldRegenerateShopItems() {
+        controller.onSceneChangedToThis();
+        verify(mockShopModel, times(1)).regenerateShopItems();
+    }
+
+    @Test
+    void onNextLevelPressed_ShouldCallShopModel() throws Exception {
+        var method = ShopController.class.getDeclaredMethod("onNextLevelPressed");
+        method.setAccessible(true);
+        method.invoke(controller);
+
+        verify(mockShopModel, times(1)).onNextLevelPressed();
+    }
+
+    @Test
+    void onUpgradeClicked_ShouldPurchase_WhenCanPurchaseTrue() throws Exception {
+        var mockUpgrade = mock(UpgradeTile.class);
+        when(mockShopModel.canPurchase(mockUpgrade)).thenReturn(true);
+
+        var method = ShopController.class.getDeclaredMethod("onUpgradeClicked", UpgradeTile.class);
+        method.setAccessible(true);
+        method.invoke(controller, mockUpgrade);
+
+        verify(mockShopModel, times(1)).purchase(mockUpgrade);
+    }
+
+    @Test
+    void onUpgradeClicked_ShouldNotPurchase_WhenCanPurchaseFalse() throws Exception {
+        var mockUpgrade = mock(UpgradeTile.class);
+        when(mockShopModel.canPurchase(mockUpgrade)).thenReturn(false);
+
+        var method = ShopController.class.getDeclaredMethod("onUpgradeClicked", UpgradeTile.class);
+        method.setAccessible(true);
+        method.invoke(controller, mockUpgrade);
+
+        verify(mockShopModel, never()).purchase(any());
+    }
+
 }
