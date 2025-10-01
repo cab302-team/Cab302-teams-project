f2a2de3f2c4027b100e3dfef136d213766569f47
Kainat-10 <punnymilkshake424@gmail.com>
2025-10-01 14:37:27 +1000

    added observe property to emptytileslotmodel

--- src/main/java/com/example/project/models/tiles/EmptyTileSlotModel.java
+++ src/main/java/com/example/project/models/tiles/EmptyTileSlotModel.java
@@ -1,4 +1,6 @@
 package com.example.project.models.tiles;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 
 /**
@@ -6,14 +8,30 @@ package com.example.project.models.tiles;
  */
 public class EmptyTileSlotModel extends Tile
 {
-    private LetterTile tile;
+    private final ObjectProperty<LetterTile> tile = new SimpleObjectProperty<>(null);
 
     /**
-     * Set a new tile.
-     * @param tile letter tile.
+     * The observable property representing the tile in this slot.
+     * @return the tile property
+     */
+    public ObjectProperty<LetterTile> tileProperty() {
+        return tile;
+    }
+
+    /**
+     * Gets the current tile in this slot.
+     * @return the LetterTile currently in this slot, or null if none
+     */
+    public LetterTile getTile() {
+        return tile.get();
+    }
+
+    /**
+     * Sets a new tile in this slot.
+     * @param tile the tile to set, or null to clear the slot
      */
     public void setTile(LetterTile tile) {
-        this.tile = tile;
+        this.tile.set(tile);
     }
 
     @Override
