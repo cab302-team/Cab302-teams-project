ad18d5161fb3a9a37a0e0f2eaf1dd59e2e7f6c48
matilda-greening <tilly.greening@outlook.com>
2025-10-14 17:38:35 +1000

    main menu model ut

---
+++ src/test/java/com/example/project/models/gameScreens/MainMenuModelTest.java
@@ -0,0 +1,40 @@
+package com.example.project.models.gameScreens;
+
+import com.example.project.models.User;
+import com.example.project.services.GameScenes;
+import com.example.project.services.SceneManager;
+import com.example.project.services.Session;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class MainMenuModelTest {
+
+    @Test
+    void onStartClicked() {
+        var mockSession = mock(Session.class);
+        var mockSceneManager = mock(SceneManager.class);
+
+        var mainMenuModel = new MainMenuModel(mockSession);
+        var sceneManager = new SceneManager(mockSceneManager);
+
+        mainMenuModel.onStartClicked();
+
+        verify(mockSceneManager).switchScene(GameScenes.LEVEL);
+    }
+
+    @Test
+    void onLogoutClicked() {
+        var mockSession = mock(Session.class);
+        var mockSceneManager = mock(SceneManager.class);
+
+        var mainMenuModel = new MainMenuModel(mockSession);
+        var sceneManager = new SceneManager(mockSceneManager);
+
+        mainMenuModel.onLogoutClicked();
+
+        verify(mockSceneManager).switchScene(GameScenes.LOGIN);
+    }
+}
